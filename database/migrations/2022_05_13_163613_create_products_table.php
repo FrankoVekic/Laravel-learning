0b7d2ed8bebe73c1a8bde4509f6b7586bbf945df
Franko Vekić <franko.vekic@gmail.com>
2022-05-13 18:55:47 +0200

    Forgot to add timestamps

--- database/migrations/2022_05_13_163613_create_products_table.php
+++ database/migrations/2022_05_13_163613_create_products_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('quantity');
             $table->decimal('price');
             $table->longText('description');
+            $table->timestamps();
         });
     }
 
